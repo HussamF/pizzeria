7ee55d8595efeb64c45d2271b98eb2f83d7631b6
HussamF <hussamfanous@gmail.com>
2021-06-05 14:43:13 +0200

    menu footer

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { PizzaMenu } from '../components';
+import Footer from '../components/Footer/Footer';
 import DesertMenu from '../components/Products/DesertMenu';
 
 const Menu = () => {
@@ -9,6 +10,7 @@ const Menu = () => {
       <MenuContainer>
         <PizzaMenu />
         <DesertMenu />
+        <Footer />
       </MenuContainer>
     </>
   );
